914e05c1fa381e508f6e97dc7ffb71904f62bc76
Ashinka Shiromal <ashinkashiromal@gmail.com>
2017-07-03 15:48:04 +0530

    Create Devise History method body

--- OpenQbit.IBMS.git/OpenQbit.IBMS.Common.Models/DeviceHistory.cs
+++ OpenQbit.IBMS.git/OpenQbit.IBMS.Common.Models/DeviceHistory.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace OpenQbit.IBMS.Common.Models
 {
-    class DeviceHistory
+    public class DeviceHistory
     {
+        public virtual Device DeviceID { set; get; }
+        public DateTime DateTime { set; get; }
+        public double Value { set; get; }
+
     }
 }

a8e72469468356c8ed87a53caac0b85c49712f70
AshanChathuskara <ashan.c4x@gmail.com>
2017-07-03 15:28:08 +0530

    Add Divice Missing Attributrs

--- OpenQbit.IBMS.git/OpenQbit.IBMS.Common.Models/Device.cs
+++ OpenQbit.IBMS.git/OpenQbit.IBMS.Common.Models/Device.cs
@@ -8,6 +8,9 @@ namespace OpenQbit.IBMS.Common.Models
 {
     public class Device
     {
+        public int ID { get; set; }
+        public String Type { get; set; }
+        public String Value { get; set; }
 
     }
 }
